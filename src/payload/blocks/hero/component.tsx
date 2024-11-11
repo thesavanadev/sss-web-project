2d774896587fee2e240dd27686c65a4f435ca7b2
Michael Caesar Owuor <michael.owuor@gmail.com>
2024-11-11 19:33:07 +0300

    refactor: modify hero component to use RenderImage function

--- src/payload/blocks/hero/component.tsx
+++ src/payload/blocks/hero/component.tsx
@@ -4,6 +4,7 @@ import { cn } from "@/lib/utils";
 
 import { Button } from "@/components/ui/button";
 import { Container } from "@/components/container";
+import { RenderImage } from "@/components/render-image";
 
 type HeroBlockProps = {
 	heroType: string;
@@ -17,20 +18,15 @@ type HeroBlockProps = {
 export const HeroBlock = ({ heroType, heroSubtitle, heroTitle, heroMessage, heroCover, heroCTA }: HeroBlockProps) => {
 	const serverURL = process.env.NODE_ENV === "development" ? process.env.NEXT_PUBLIC_SERVER_URL_DEV! : process.env.NEXT_PUBLIC_SERVER_URL_PRD!;
 	const src = heroCover && typeof heroCover === "object" ? heroCover.url : null;
+	const alt = heroCover && typeof heroCover === "object" ? heroCover.alt : null;
 
 	return (
 		<section className="m-1">
-			<div
-				className={cn("relative left-0 top-0 flex w-full items-center justify-center rounded-lg bg-cover bg-center", {
-					"h-[50rem]": heroType === "homepage",
-					"h-[30rem]": heroType === "subpage",
-				})}
-				style={{
-					backgroundImage: `url(${serverURL}${src})`,
-					aspectRatio: "auto",
-				}}
-			>
-				<div className="absolute inset-0 bg-gradient-to-r from-black/90 to-black/50" />
+			<div className={cn("relative flex items-center", { "h-[50rem]": heroType === "homepage", "h-[30rem]": heroType === "subpage" })}>
+				<div className="absolute inset-0 z-0">
+					<RenderImage src={`${serverURL}${src}`} alt={`${serverURL}${alt}`} />
+					<div className="absolute inset-0 bg-gradient-to-r from-black/90 to-black/50" />
+				</div>
 
 				<Container className="z-10 text-white">
 					<div className="max-w-2xl space-y-6 text-pretty">
